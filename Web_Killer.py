9d1f3db118a52b47381500d695f9bbad2f1b25aa
faiyazahmad07 <63125338+faiyazahmad07@users.noreply.github.com>
2020-05-21 00:54:26 +0530

    Update Web_Killer.py

--- Web_Killer.py
+++ Web_Killer.py
@@ -36,7 +36,7 @@ def web_finder(val):
     with open(val, "r") as websites:
         for items in websites:
             items = items.strip()
-            if items[0:8] != 'http://' or item[0:8] != 'http://':
+            if items[0:8] != 'http://' or item[0:8] != 'https://':
                 items = 'http://' + items
                 try:
                     response = requests.get(items, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
